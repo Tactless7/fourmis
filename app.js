61371d8e1ce70f1a8b58e5de03166497c3b62f86
Juliane Blier <blier.juliane@gmail.com>
2016-10-07 15:06:11 +0200

    Apparition automatique d'une fourmi dans le tableau

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 /*
 	- Générer un tableau selon paramètres OK
 		récupérer dimensions via input OK
-	- Créer un déplacement aléatoire
+		donner des coordsonnées au tableau automatiquement OK
+	- Créer l'apparition aléatoire de la fourmi
+		random coordonées
+		assigner les random coordonées à la fourmi
+	- Créer le déplacement aléatoire
+
 		
 */
 var width;
 var height;
-
+var RandomRowFourmi;
+var RandomColumnFourmi;
+	
+	// Tableau selon les valeurs de l'input
 function createTable(x, y){
 	for (var i = 1; i <= y; i++) {
 		$('table').append('<tr data-row ="'+i+'""></tr>');
@@ -16,13 +24,30 @@ function createTable(x, y){
 	}
 }
 
-function randomCase
+	// Génère des coordonnées pour l'apparition de la fourmi
+function randomCase(){
+	RandomRowFourmi = Math.floor(Math.random() * height) + 1;
+	RandomColumnFourmi = Math.floor(Math.random() * width) + 1;
+	console.log(RandomColumnFourmi + ', ' + RandomRowFourmi);
+}
 
+	// Création de la fourmi qui apparait aléatoirement dans le tableau
+function createFourmi(){
+	randomCase();
+	$('td[data-row="'+RandomRowFourmi+'"][data-column="'+RandomColumnFourmi+'"]').text('#');
+}
 
+  // Au moment du clic : génération du monde
 $('button').on('click', function(){
 	$('table').html('');
-	width = $('#x').val();
-	height = $('#y').val();
+	width = parseInt($('#x').val());
+	height = parseInt($('#y').val());
 	createTable(width, height);
+	createFourmi();
 
+	setInterval(function(){
+		
+
+		
+	}, 1000);
 });
